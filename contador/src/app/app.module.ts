c8b4578e13db15cea2d80081ff3cbe7b3ba8b8dd
Juan Cruz Agüero Luna <jcaguerodev@gmail.com>
2023-03-06 08:27:23 -0300

    Login, registro y primeras rutas

--- contador/src/app/app.module.ts
+++ contador/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,

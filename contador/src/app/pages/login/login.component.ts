961ff4d31356a0779a49118561b24511cd0d4c26
Juan Cruz Agüero Luna <jcaguerodev@gmail.com>
2023-03-07 19:48:03 -0300

    Obteniendo valores del login

--- contador/src/app/pages/login/login.component.ts
+++ contador/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 //declare var JQuery: any;
 //declare var $: any;
@@ -10,5 +11,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  formSubmit: boolean = false;
+
+  public loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  constructor(private fb:FormBuilder){}
+
+  login(){
+    this.formSubmit = true;
+
+    if(this.loginForm.invalid){
+      return;
+    }
+
+    console.log(this.loginForm.value)
+  }
   
 }

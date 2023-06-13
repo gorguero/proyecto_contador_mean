2fad60df90a2193fcb39652543c9965f316f8f9f
gorguero <jcaguerodev@gmail.com>
2023-06-13 17:36:25 -0300

    validaciones de formulario y agregando info del usuario al componente

--- contador/src/app/pages/perfil/perfil.component.ts
+++ contador/src/app/pages/perfil/perfil.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit{
+
+  public perfilForm!: FormGroup;
+  public usuario: any;
+
+  constructor(private usuarioService:UsuarioService, private fb:FormBuilder){
+    this.usuario = usuarioService.usuario;
+  }
+
+  ngOnInit(): void {
+    this.perfilForm = this.fb.group({
+      nombre: [ this.usuario.nombre, Validators.required ],
+      email: [ this.usuario.email, [Validators.required, Validators.email] ],
+      curp: [ this.usuario.curp, Validators.required ],
+      telefono: [ this.usuario.telefono, Validators.required ]
+    })
+  }
+
+  actualizar(){
+    console.log(this.perfilForm.value)
+  }
 
 }

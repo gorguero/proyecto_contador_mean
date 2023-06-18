a192a1cecc0e43b57bb9ad342fb5229ff7f918b6
gorguero <jcaguerodev@gmail.com>
2023-06-18 20:08:00 -0300

    Creando los siguientes componentes a utilizar

--- contador/src/app/pages/pages.module.ts
+++ contador/src/app/pages/pages.module.ts
@@ -4,11 +4,23 @@ import { RouterModule } from '@angular/router'; //Permite navegación entre comp
 import { PagesComponent } from './pages.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PerfilComponent } from './perfil/perfil.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { NuevoDocumentoComponent } from './nuevo-documento/nuevo-documento.component';
+import { EditarDocumentoComponent } from './editar-documento/editar-documento.component';
+import { DocumentosComponent } from './documentos/documentos.component';
+import { ArchivosComponent } from './archivos/archivos.component';
+import { MisDocumentosComponent } from './mis-documentos/mis-documentos.component';
 
 @NgModule({
   declarations: [
     PagesComponent,
     PerfilComponent,
+    UsuariosComponent,
+    NuevoDocumentoComponent,
+    EditarDocumentoComponent,
+    DocumentosComponent,
+    ArchivosComponent,
+    MisDocumentosComponent,
   ],
   imports: [
     CommonModule,

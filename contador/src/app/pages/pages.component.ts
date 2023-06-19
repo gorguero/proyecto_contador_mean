9d71d3a472eaac715c68bb1e03e376b7285e5fdf
gorguero <jcaguerodev@gmail.com>
2023-06-19 12:33:05 -0300

    Ajuste en el menú

--- contador/src/app/pages/pages.component.ts
+++ contador/src/app/pages/pages.component.ts
@@ -11,6 +11,29 @@ export class PagesComponent implements OnInit {
 
   public usuario: any;
 
+  menu: any[] = [
+    {
+      titulo: 'Mi Perfil',
+      icono: 'mdi mdi-folder-lock-open',
+      url: '/dashboard/perfil'
+    },
+    {
+      titulo: 'Usuarios',
+      icono: 'mdi mdi-folder-lock-open',
+      url: '/dashboard/usuarios'
+    },
+    {
+      titulo: 'Nuevo Documento',
+      icono: 'mdi mdi-folder-lock-open',
+      url: '/dashboard/nuevo-documento'
+    },
+    {
+      titulo: 'Documentos Generales',
+      icono: 'mdi mdi-folder-lock-open',
+      url: '/dashboard/documentos'
+    },
+  ];
+
   constructor( private router:Router, private usuarioService:UsuarioService) {    
     this.usuario = usuarioService.usuario;
     console.log(this.usuario);

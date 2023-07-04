5410544f1bab1c9b74bb4f5f7a58dcbe24942c9c
gorguero <jcaguerodev@gmail.com>
2023-07-04 10:33:29 -0300

    cargando informacion
    Error al obtener usuario, se revisara mas adelante

--- contador/src/app/pages/mis-documentos/mis-documentos.component.ts
+++ contador/src/app/pages/mis-documentos/mis-documentos.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DocumentosService } from 'src/app/services/documentos.service';
 
 @Component({
   selector: 'app-mis-documentos',
   templateUrl: './mis-documentos.component.html',
   styleUrls: ['./mis-documentos.component.css']
 })
-export class MisDocumentosComponent {
+export class MisDocumentosComponent implements OnInit{
 
+  public cargando: boolean = false;
+  public documentos!: any[];
+
+  constructor(private activatedRoute: ActivatedRoute, private documentosService: DocumentosService){
+    
+  }
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe( ({id}) => {
+      this.cargarDocumentosPersonalesID( id );
+    } )
+  }
+
+  cargarDocumentosPersonalesID( id:string ){
+    this.cargando = true;
+    this.documentosService.cargarMisDocumentosPersonales(id)
+      .subscribe( (documentos) => {
+        this.documentos = documentos;
+        console.log(this.documentos)
+        this.cargando = false;
+      } )
+  }
+  
 }

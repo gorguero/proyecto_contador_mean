7b7531c42fdf3f3794be47f6f1ff8ad15937bd6d
gorguero <jcaguerodev@gmail.com>
2023-07-04 09:30:55 -0300

    Creando modelo de documentos

---
+++ contador/src/app/models/documentos.models.ts
@@ -0,0 +1,11 @@
+export class Documentos{
+    
+    constructor(
+        public nombre: string,
+        public fecha?: string,
+        public usuario?: string,
+        public pdf?: string,
+        public _id?: string,
+    ){}
+
+}

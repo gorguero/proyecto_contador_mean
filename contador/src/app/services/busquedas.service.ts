5b5d905d94d25403c8bd38249fe34a4ad492fd9b
gorguero <jcaguerodev@gmail.com>
2023-07-11 18:55:26 -0300

    Buscar service

---
+++ contador/src/app/services/busquedas.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import Usuarios from '../models/usuarios.model';
+import { Documentos } from '../models/documentos.models';
+
+const url = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusquedasService {
+
+  constructor(private http:HttpClient) { }
+
+  get token():string{
+    return localStorage.getItem('token') || '';
+  }
+  get headers(){
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
+
+  private buscarUsuarios( resultados:any[] ):Usuarios[]{
+    return resultados.map(
+      user => new Usuarios(user.nombre, user.email, user.curp, user.telefono, '', '', user.rol, user.uid)
+    );
+  }
+  private buscarDocumentos( resultados:any[] ):Documentos[]{
+    return resultados.map(
+      doc => new Documentos(doc.nombre, doc.fecha, doc.usuario, doc.pdf, doc._id)
+    );
+  }
+
+  buscar(tipo: 'usuarios' | 'documentos', termino:string){
+    const urlBuscar = `${url}/coleccion/tipo/termino`;
+    this.http.get<any[]>(urlBuscar, this.headers)
+      .pipe(
+        map( (resp:any):any => {
+          switch(tipo){
+            case 'usuarios':
+              return this.buscarUsuarios(resp);
+            case 'documentos':
+              return this.buscarDocumentos(resp);
+
+            default: [];
+          }
+        })
+      );
+  }
+  
+}
